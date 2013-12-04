92f526f4223bbe6569cd5ebf60dafcb1c18aafca
Chris Lapa <chris@lapa.com.au>
2013-12-04 17:24:12 +1100

    Project: Updated assembly info and some project settings.
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

--- cPanelDDNS/Properties/AssemblyInfo.cs
+++ cPanelDDNS/Properties/AssemblyInfo.cs
@@ -4,17 +4,17 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 [assembly: AssemblyTitle ("cPanelDDNS")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("cPanel Dynamic DNS Client")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("Chris")]
+[assembly: AssemblyProduct ("cPanel Dynamic DNS Client")]
+[assembly: AssemblyCopyright ("GusBricker")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("1.3.*")]
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
