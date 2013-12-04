503c2ef55000b29edfd94ef248d0dbd9f3a6323a
Chris Lapa <chris@lapa.com.au>
2013-12-04 17:20:28 +1100

    Utils: Public IP: Catching WebRequest exceptions.
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

--- Utils/PublicIP.cs
+++ Utils/PublicIP.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Text.RegularExpressions;
 using System.IO;
 using System.Text;
+using Utils.Logging;
 
 namespace Utils.IP
 {
@@ -10,15 +11,23 @@ namespace Utils.IP
 	{
 		public static string Lookup()
 		{
-			WebRequest req = WebRequest.Create ("http://checkip.dyndns.org:8245");
-			req.Timeout = 2000;
+			try
+			{
+				WebRequest req = WebRequest.Create ("http://checkip.dyndns.org:8245");
+				req.Timeout = 2000;
 
-			HttpWebResponse webResp = (HttpWebResponse)req.GetResponse();
-			StreamReader reader = new StreamReader (webResp.GetResponseStream (), Encoding.UTF8);
-			string response = reader.ReadToEnd ();
-		    response = (new Regex(@"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")).Match(response).Value;
+				HttpWebResponse webResp = (HttpWebResponse)req.GetResponse();
+				StreamReader reader = new StreamReader (webResp.GetResponseStream (), Encoding.UTF8);
+				string response = reader.ReadToEnd ();
+			    response = (new Regex(@"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")).Match(response).Value;
 
-		    return response;
+			    return response;
+			}
+			catch (Exception e)
+			{
+				Logger.Instance.Write (Logger.Severity.Error, "IP Address lookup failed, ensure firewall hasnt blocked port 8245, exception: " + e.Message);
+				return string.Empty;
+			}
 		}
 	}
 }
