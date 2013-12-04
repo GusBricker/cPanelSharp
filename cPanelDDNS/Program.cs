19a3ad831607ec756c2463acbf03795f7dc70fe4
Chris Lapa <chris@lapa.com.au>
2013-12-04 17:23:46 +1100

    cPanelDDNS: Better use of exit codes, also added help for exit codes.
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

--- cPanelDDNS/Program.cs
+++ cPanelDDNS/Program.cs
@@ -11,10 +11,26 @@ using CommandLine;
 using Utils.Logging;
 using Utils.IP;
 
-namespace cPanelAPILib
+namespace cPanelDDNS
 {
 	class MainClass
 	{
+		private enum DDNSResult
+		{
+			Error,
+			NotNeccessary,
+			UpdateSuccess
+		}
+
+		private enum DDNSReturnCodes
+		{
+			NoUpdateNeccessary,
+			UpdateSuccess,
+			InvalidOptionsGiven,
+			IPLookupFailure,
+			GeneralError
+		}
+
 		public static void Main (string[] args)
 		{
             string logPath = Path.GetTempPath();
@@ -28,12 +44,13 @@ namespace cPanelAPILib
 			CmdOptions options = new CmdOptions ();
 			if (CommandLine.Parser.Default.ParseArguments (args, options) == false)
 			{
-				Console.WriteLine ("Invalid options given");
-				Environment.Exit (1);
+				Console.WriteLine ("Return codes: ");
+				Console.WriteLine (BuildReturnString ());
+				Environment.Exit ((int)DDNSReturnCodes.InvalidOptionsGiven);
 			}
 
 			// Check if we need to lookup our public IP on the internet
-			if (options.IP == string.Empty)
+			if (string.IsNullOrEmpty (options.IP))
 			{
 				Logger.Instance.Write (Logger.Severity.Debug, 
 				                       "No IP given on the command line, looking up");
@@ -42,7 +59,7 @@ namespace cPanelAPILib
 				if (options.IP == string.Empty)
 				{
 					Logger.Instance.Write (Logger.Severity.Error, "IP Address lookup failed");	
-					Environment.Exit (3);
+					Environment.Exit ((int)DDNSReturnCodes.IPLookupFailure);
 				}
 
 				Logger.Instance.Write (Logger.Severity.Debug, 
@@ -51,37 +68,43 @@ namespace cPanelAPILib
 			                                                
 
 			// Setup the cPanelAPI
-			bool pass;
 			cPanelAPI api = new cPanelAPI (options.URL,
 			                               options.Username,
 			                               options.Password,
 			                               options.Secure);
 
 			// Update the domain A record only, point to our IP.
-			pass = UpdateDomainARecord (api,
-			                            options.Domain,
-			                            options.Zone,
-			                            options.IP);
+			DDNSResult result;
+			result = UpdateDomainARecord (api,
+			                              options.Domain,
+			                              options.Zone,
+			                              options.IP);
 
-			if (pass == true)
+			if (result == DDNSResult.NotNeccessary)
+			{
+				Logger.Instance.Write (Logger.Severity.Debug, 
+									   "IP address in record same as given IP, nothing to do");
+				Environment.Exit ((int)DDNSReturnCodes.NoUpdateNeccessary);
+			}
+			else if (result == DDNSResult.UpdateSuccess)
 			{
 				Logger.Instance.Write (Logger.Severity.Debug,
 				                       "Successfully updated zone " + options.Zone +
 									   " on domain " + options.Domain + 
 									   " to point to IP " + options.IP);
+				Environment.Exit ((int)DDNSReturnCodes.UpdateSuccess);
 			}
 			else
 			{
 				Logger.Instance.Write (Logger.Severity.Error, "Updating zone record has failed");
-				Environment.Exit (2);
+				Environment.Exit ((int)DDNSReturnCodes.GeneralError);
 			}
 		}
 
-
-		public static bool UpdateDomainARecord (cPanelAPI api,
-		                                        string domain,
-		                                        string zone,
-		                                        string ip)
+		private static DDNSResult UpdateDomainARecord (cPanelAPI api,
+			                                          string domain,
+			                                          string zone,
+			                                          string ip)
 		{
 			cPanelZoneEditFunctions funcs = new cPanelZoneEditFunctions (api);
 			FetchZoneResponse zoneInfo;
@@ -99,24 +122,45 @@ namespace cPanelAPILib
 				catch (Exception)
 				{
 					Logger.Instance.Write (Logger.Severity.Error, "Received record line is not an integer");
-					return false;
+					return DDNSResult.Error; 
 				}
 
 				Logger.Instance.Write (Logger.Severity.Debug, "Given IP: " + ip);
 				Logger.Instance.Write (Logger.Severity.Debug, "Zone IP: " + zoneInfo.Zone.Address);
 				if (ip == zoneInfo.Zone.Address)
 				{
-					Logger.Instance.Write (Logger.Severity.Debug, "IP address in record same as given IP, nothing to do");
-					return true;
+					return DDNSResult.NotNeccessary;
 				}
 
 				if (funcs.EditZoneARecord (domain, ip, recordLine, out zoneResponse) == true)
 				{
-					return zoneResponse.Status == "1";
+					if (zoneResponse.Status == "1")
+					{
+						return DDNSResult.UpdateSuccess;
+					}
+					else
+					{
+						return DDNSResult.Error;
+					}
 				}
 			}
 
-			return false;
+			return DDNSResult.Error;
+		}
+
+		private static string BuildReturnString ()
+		{
+			StringBuilder sb = new StringBuilder ();
+
+			foreach (DDNSReturnCodes code in Enum.GetValues (typeof(DDNSReturnCodes)))
+			{
+				sb.Append ((int)code);
+				sb.Append (": ");
+				sb.Append (code.ToString ());
+				sb.AppendLine ();
+			}
+
+			return sb.ToString ();
 		}
 
 //		public static bool UpdateDomainCName (cPanelAPI api,
